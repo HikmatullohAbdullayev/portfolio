fe1e721e92ba1924f2f41516216bfe7dbaf7677f
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2024-04-25 14:46:43 +0500

    dark theme done

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
+
 
 
 function Layout() {
+    const {dark} = useContext(ThemeContext)
     return (
-        <div className="wrapper">
+        <div className={`wrapper  ${dark ? "bg-[#1E1E2E]" : "bg-white"}`}>
             <div className="box">
             <div className="container">
             <Header/>
